cefb26a6223569a54fde29e1e28f9f250efad70b
ewelinagr <ewelina@thehyve.nl>
2016-08-24 09:16:22 +0200

    Change in versioning of the rest-api, functional tests fix.
    TRANSUI-165

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,13 @@
 class UrlMappings {
 
 	static mappings = {
-		"/v1/$controller/$action?/$id?"{
+		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
 
-		"/v1/"(view:"/index")
+		"/"(view:"/index")
 		"500"(view:'/error')
 	}
 }
